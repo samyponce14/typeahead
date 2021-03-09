c455628ce358437fc00630f804c8070b484108c2
Samantha Ponce <samy.ponce14@gmail.com>
2021-03-09 17:18:25 +0100

    Add main test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,25 +1,12 @@
 import * as React from 'react';
+import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 
 import { App } from './App';
 
-import { render, fireEvent, screen } from '@testing-library/react';
-
 describe('App', () => {
-  it('renders ansd increments counter', () => {
-    const { getByText, getByTitle, asFragment } = render(
-      <App initialData={{ appName: 'TEST' }} />,
-    );
-    expect(getByText('TEST')).toMatchInlineSnapshot(`
-      <h1>
-        TEST
-      </h1>
-    `);
-
-    const button = screen.getByTitle('increment');
-    fireEvent.click(button);
-    expect(getByTitle('increment')).toHaveTextContent('1');
-
-    expect(asFragment()).toMatchSnapshot();
+  it('renders App with TypeaheadWidget', () => {
+    const tree = renderer.create(<App />);
+    expect(tree).toMatchSnapshot();
   });
 });
--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -3,16 +3,20 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const SuggestionList = ({ suggestions = [], handleSelectSuggestion, highlightedLetters = '', cursor, refs }) => {
+const SuggestionList = ({ suggestions = [], handleSelectSuggestion, highlightedLetters = '', cursor, refs = [] }) => {
 
 	const getHighlightedText = (text, highlight) => {
 		// Split on highlight term and include term into parts, ignore case
 		const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
-		return <span> {parts.map((part, i) =>
-			<span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { fontWeight: 'bold' } : {}}>
-				{part}
-			</span>)
-		} </span>;
+		return (
+			<span>
+				{ parts.map((part, i) =>
+					<span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { fontWeight: 'bold' } : {}}>
+						{part}
+					</span>)
+				}
+			</span>
+		);
 	};
 
 	return (
